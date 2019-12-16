d0598d8d0dc5dc689ec775771c9bdc25c2f60174
Ankit Sharma <ankit.sharma@storynes.com>
2019-12-16 12:22:33 +0530

    Debounce function for better searching

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hi there!');
+const fetchData = async searchTerm => {
+  const respose = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "87c8fe40",
+      s: searchTerm
+    }
+  });
+  console.log(respose.data);
+};
+
+const input = document.querySelector("input");
+
+const onInput = e => {
+  fetchData(e.target.value);
+};
+input.addEventListener("input", debounce(onInput, 500));
---
+++ utils.js
@@ -0,0 +1,12 @@
+// Debounce function for making a search ificiently
+let debounce = (func, delay = 1000) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
+};
