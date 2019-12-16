5eb14d8d74cb77f041fe5729428ae01367417ed3
Ankit Sharma <ankit.sharma@storynes.com>
2019-12-16 15:57:50 +0530

    Adding the functinality for rendring the data on screen

--- index.js
+++ index.js
@@ -11,20 +11,65 @@ const fetchData = async searchTerm => {
   return respose.data.Search;
 };
 
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+<label><b>Search For a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+      <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+      </div>
+    </div>
+`;
+
 const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultWrapper = document.querySelector(".results");
 
 const onInput = async e => {
   const movies = await fetchData(e.target.value);
 
-  for (let movie of movies) {
-    const div = document.createElement("div");
+  if (!movies.length) {
+    dropdown.classList.remove("is-active");
+    return;
+  }
 
-    div.innerHTML = `
-    <img src="${movie.Poster}"/>
+  resultWrapper.innerHTML = "";
+  dropdown.classList.add("is-active");
+  for (let movie of movies) {
+    const option = document.createElement("a");
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
+    <img src="${imgSrc}"/>
     <h1>${movie.Title}</h1>
     `;
+
+    option.addEventListener("click", () => {
+      dropdown.classList.remove("is-active");
+      input.value = movie.Title;
+      onMovieSelect(movie);
+    });
+
     // Adding to the DOM
-    document.querySelector("#target").appendChild(div);
+    resultWrapper.appendChild(option);
   }
 };
-input.addEventListener("input", debounce(onInput, 500));
+input.addEventListener("input", debounce(onInput, 100));
+
+document.addEventListener("click", e => {
+  if (!root.contains(e.target)) {
+    dropdown.classList.remove("is-active");
+  }
+});
+
+const onMovieSelect = async movie => {
+  const respose = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "87c8fe40",
+      i: movie.imdbID
+    }
+  });
+  console.log(respose.data);
+};
